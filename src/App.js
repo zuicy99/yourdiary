576c90cc317c8b0ce18fa7320ed43827df5b6386
zuicy99 <k.zuicy@gmail.com>
2023-11-30 18:46:24 +0900

    docs : eslint, 초기환경 구축

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import styled from "@emotion/styled";
+
 function App() {
-  return <div>테스트</div>;
+  const TopBar = styled.header``;
+  return (
+    <div className="layout">
+      <div className="wrap">
+        <TopBar>
+          <button>
+            <img src="images/tbt_list.svg" />
+          </button>
+        </TopBar>
+      </div>
+    </div>
+  );
 }
 
 export default App;
